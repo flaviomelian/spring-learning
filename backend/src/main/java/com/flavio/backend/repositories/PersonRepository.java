02afb137805266f1f99b7cb635fb8aa0362f9b94
flaviomelian <flaviomelian2000@gmail.com>
2025-03-16 20:26:51 +0000

    DELETE funcional

--- backend/src/main/java/com/flavio/backend/repositories/PersonRepository.java
+++ backend/src/main/java/com/flavio/backend/repositories/PersonRepository.java
@@ -2,5 +2,7 @@ package com.flavio.backend.repositories;
 
 import com.flavio.backend.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {}
